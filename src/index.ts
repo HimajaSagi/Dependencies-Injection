6985a04a65e723aefd8673576ee8fd54d81595e3
LSAGI1 <lsagi1@ford.com>
2026-02-23 12:24:36 +0530

    feature(ioc): implement ioc container

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import { Users } from './services/users';
-import { Logger } from './services/logger';
 
 import type { User, ApiConfig } from './types';
+import { createIoCContainer } from './ioc';
 
-const renderUsers = async (config: ApiConfig) => {
-  const usersService = new Users(config);
+const renderUsers = async (usersService: any) => {
   const users = await usersService.getUsers();
 
   const listNode = document.getElementById('users-list');
@@ -20,14 +18,13 @@ const renderUsers = async (config: ApiConfig) => {
 const app = () => {
   const config = (window as any).__CONFIG__;
   delete (window as any).__CONFIG__;
-
-  renderUsers(config.api);
+  const ioc = createIoCContainer(config.api);
+  const logger = ioc.resolve('logger');
+  logger.info('Page is loaded.');
+  const usersService = ioc.resolve('users');
+  renderUsers(usersService);
 };
 
 window.onload = (event: Event) => {
-  const logger = new Logger();
-
-  logger.info('Page is loaded.');
-
   app();
 };
--- src/ioc/index.ts
+++ src/ioc/index.ts
@@ -1,12 +1,15 @@
+
+import { Logger } from '../services/logger';
+import { HTTP } from '../services/http';
+import { Users } from '../services/users';
+import type { ApiConfig } from '../types';
 import IoCContainer from 'ioc-lite';
 
-// import { Logger } from '../services/logger';
-// import { HTTP } from '../services/http';
-// import { Users } from '../services/users';
-
-export const createIoCContainer = () =>  {
+export const createIoCContainer = (apiConfig: ApiConfig) =>  {
   const ioc = new IoCContainer();
-  // you can register some resources right now below...
-
+  ioc.registerClass('logger', Logger);
+  ioc.registerClass('http', HTTP);
+  ioc.registerClass('users', Users);
+  ioc.register('apiConfig', apiConfig);
   return ioc;
 };
